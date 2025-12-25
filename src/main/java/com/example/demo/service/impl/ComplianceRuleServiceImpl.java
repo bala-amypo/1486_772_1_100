49bb129ff88357c5c6b551f0c598e1513e87ec33
bala-amypo <bala@amypo.in>
2025-12-25 13:34:01 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/ComplianceRuleServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.exception.ValidationException;
+import com.example.demo.model.ComplianceRule;
+import com.example.demo.repository.ComplianceRuleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ComplianceRuleServiceImpl implements ComplianceRuleService {
+
+    private final ComplianceRuleRepository complianceRuleRepository;
+
+    public ComplianceRuleServiceImpl(ComplianceRuleRepository complianceRuleRepository) {
+        this.complianceRuleRepository = complianceRuleRepository;
+    }
+
+    @Override
+    public ComplianceRule createRule(ComplianceRule rule) {
+
+        if (rule.getRuleName() != null &&
+                complianceRuleRepository.findAll().stream()
+                        .anyMatch(r -> rule.getRuleName().equals(r.getRuleName()))) {
+            throw new ValidationException("Duplicate rule name");
+        }
+
+        return complianceRuleRepository.save(rule);
+    }
+
+    @Override
+    public List<ComplianceRule> getAllRules() {
+        return complianceRuleRepository.findAll();
+    }
+
+    @Override
+    public ComplianceRule getRule(Long id) {
+        return complianceRuleRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Rule not found"));
+    }
+}
