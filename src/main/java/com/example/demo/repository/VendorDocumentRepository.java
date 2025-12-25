a562e8a0fda55cb51384c87191982ed54c59e760
bala-amypo <bala@amypo.in>
2025-12-25 15:02:02 +0000

    User commit

--- src/main/java/com/example/demo/repository/VendorDocumentRepository.java
+++ src/main/java/com/example/demo/repository/VendorDocumentRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Vendor;
 import com.example.demo.model.VendorDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,6 +8,8 @@ import java.util.List;
 
 public interface VendorDocumentRepository extends JpaRepository<VendorDocument, Long> {
 
-    // REQUIRED by ComplianceScoreService + tests
     List<VendorDocument> findByVendorId(Long vendorId);
+
+    // ✅ REQUIRED
+    List<VendorDocument> findByVendor(Vendor vendor);
 }
