eea13ac71e42a9ef0795636bc6ef0a1c70569a75
bala-amypo <bala@amypo.in>
2025-12-25 14:34:04 +0000

    User commit

--- src/main/java/com/example/demo/repository/VendorDocumentRepository.java
+++ src/main/java/com/example/demo/repository/VendorDocumentRepository.java
@@ -1,3 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Vendor;
+import com.example.demo.model.VendorDocument;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface VendorDocumentRepository extends JpaRepository<VendorDocument, Long> {
 
     List<VendorDocument> findByVendor_Id(Long vendorId);
@@ -7,3 +17,4 @@ public interface VendorDocumentRepository extends JpaRepository<VendorDocument, 
     @Query("SELECT vd FROM VendorDocument vd WHERE vd.expiryDate < :cutoffDate")
     List<VendorDocument> findExpiredDocuments(LocalDate cutoffDate);
 }
+
