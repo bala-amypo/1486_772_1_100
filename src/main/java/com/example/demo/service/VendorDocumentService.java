36020b26db4683124df35bb1cf45bc7467bf2f4b
bala-amypo <bala@amypo.in>
2025-12-26 04:09:53 +0000

    User commit

--- src/main/java/com/example/demo/service/VendorDocumentService.java
+++ src/main/java/com/example/demo/service/VendorDocumentService.java
@@ -1,8 +1,13 @@
 package com.example.demo.service;
 
 import com.example.demo.model.VendorDocument;
+import java.util.List;
 
 public interface VendorDocumentService {
 
-    VendorDocument uploadDocument(VendorDocument document);
+    VendorDocument uploadDocument(Long vendorId, Long typeId, VendorDocument document);
+
+    List<VendorDocument> getDocumentsForVendor(Long vendorId);
+
+    VendorDocument getDocument(Long documentId);
 }
