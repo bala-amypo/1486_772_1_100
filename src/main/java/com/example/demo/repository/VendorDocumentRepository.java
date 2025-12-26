2b51e3fbc0d9b1ab5d0c192f331271f873c57c56
bala-amypo <bala@amypo.in>
2025-12-26 05:14:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/VendorDocumentRepository.java
+++ src/main/java/com/example/demo/repository/VendorDocumentRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
 import java.util.List;
 
-public interface VendorDocumentRepository
-        extends JpaRepository<VendorDocument, Long> {
+public interface VendorDocumentRepository extends JpaRepository<VendorDocument, Long> {
 
     List<VendorDocument> findByExpiryDateBefore(LocalDate date);
 }
