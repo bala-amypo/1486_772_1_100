733d5c4a5f224501114ec095b937c690c06f9d1c
bala-amypo <bala@amypo.in>
2025-12-26 05:10:29 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -13,10 +13,24 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String role;
 
+    public User() {}
+
+    public User(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    // getters & setters
     public Long getId() { return id; }
     public String getEmail() { return email; }
     public String getPassword() { return password; }
