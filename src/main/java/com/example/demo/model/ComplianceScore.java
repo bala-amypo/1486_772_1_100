847e5b7e5ff1abab7283d5a52a0fe2969c449fdd
bala-amypo <bala@amypo.in>
2025-12-26 05:13:15 +0000

    User commit

--- src/main/java/com/example/demo/model/ComplianceScore.java
+++ src/main/java/com/example/demo/model/ComplianceScore.java
@@ -14,14 +14,16 @@ public class ComplianceScore {
     private Vendor vendor;
 
     private double score;
+
     private String rating;
 
-    public Long getId() { return id; }
+    public ComplianceScore() {}
+
+    // getters & setters
     public Vendor getVendor() { return vendor; }
     public double getScore() { return score; }
     public String getRating() { return rating; }
 
-    public void setId(Long id) { this.id = id; }
     public void setVendor(Vendor vendor) { this.vendor = vendor; }
     public void setScore(double score) { this.score = score; }
     public void setRating(String rating) { this.rating = rating; }
