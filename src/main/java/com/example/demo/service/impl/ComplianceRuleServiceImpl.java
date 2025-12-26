8bcfa2e69e13afcfff02cadcc0ca91a6aaa6c27c
bala-amypo <bala@amypo.in>
2025-12-26 05:18:48 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ComplianceRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ComplianceRuleServiceImpl.java
@@ -1,41 +1,21 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.exception.ValidationException;
 import com.example.demo.model.ComplianceRule;
 import com.example.demo.repository.ComplianceRuleRepository;
 import com.example.demo.service.ComplianceRuleService;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
 @Service
 public class ComplianceRuleServiceImpl implements ComplianceRuleService {
 
-    private final ComplianceRuleRepository complianceRuleRepository;
+    private final ComplianceRuleRepository repository;
 
-    public ComplianceRuleServiceImpl(ComplianceRuleRepository complianceRuleRepository) {
-        this.complianceRuleRepository = complianceRuleRepository;
+    public ComplianceRuleServiceImpl(ComplianceRuleRepository repository) {
+        this.repository = repository;
     }
 
     @Override
-    public ComplianceRule createRule(ComplianceRule rule) {
-        List<ComplianceRule> existingRules = complianceRuleRepository.findAll();
-        for (ComplianceRule r : existingRules) {
-            if (r.getRuleName().equalsIgnoreCase(rule.getRuleName())) {
-                throw new ValidationException("Duplicate rule name");
-            }
-        }
-        return complianceRuleRepository.save(rule);
-    }
-
-    @Override
-    public List<ComplianceRule> getAllRules() {
-        return complianceRuleRepository.findAll();
-    }
-
-    @Override
-    public ComplianceRule getRule(Long id) {
-        return complianceRuleRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("ComplianceRule not found"));
+    public ComplianceRule save(ComplianceRule rule) {
+        return repository.save(rule);
     }
 }
