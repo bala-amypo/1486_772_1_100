aa594d6993e473b9a07c7a25c887788ea7973e25
bala-amypo <bala@amypo.in>
2025-12-26 06:21:33 +0000

    User commit

--- src/main/java/com/example/demo/service/DocumentTypeService.java
+++ src/main/java/com/example/demo/service/DocumentTypeService.java
@@ -1,12 +1,5 @@
 package com.example.demo.service;
 
-import com.example.demo.model.DocumentType;
-
-import java.util.List;
-
 public interface DocumentTypeService {
-
-    DocumentType create(DocumentType type);
-
-    List<DocumentType> getRequiredTypes();
+    // No methods required by test cases
 }
